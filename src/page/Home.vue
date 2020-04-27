d9322d4d1c9f6c004088d6274478e7ec81412307
‘aiyuya’ <EchoTravel7@163.com>
2020-04-27 15:27:20 +0800

    low啊

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -20,6 +20,20 @@
             <span @click.prevent="exit">退出</span>
             </div>
         </div>
+        <div class="msgmoudel">
+            <el-button class="changemsg" @click.prevent="show">换一组</el-button>
+            <div style="display: flex; margin-top: 20px; height: 100px;">
+                <transition name="el-zoom-in-center">
+                    <div v-show="show3" class="transition-box">{{box1}}</div>
+                </transition>
+                <transition name="el-zoom-in-top">
+                    <div v-show="show3" class="transition-box">{{box2}}</div>
+                </transition>
+                <transition name="el-zoom-in-bottom">
+                    <div v-show="show3" class="transition-box">{{box3}}</div>
+                </transition>
+            </div>
+        </div>
     </div>  
 </template>
 <script>
@@ -27,13 +41,26 @@
         data(){
             return{
                msg:'home',
-               user:''
+               user:'',
+               isshowmessage:false,
+               show3: true,
+               box1:'2',
+               box2:'1',
+               box3:'1',
             }
         },
         created () {
             var data = this.$cookieStore.getCookie('username')
             if(data){
                 this.user = data;
+                this.$http.get('data')
+                .then(res=>{
+                    this.box1 = res['data'][0]
+                    this.box2 = res['data'][1]
+                    this.box3 = res['data'][2]
+                })  
+                .catch(err=>{
+                })
             }else{
                this.$router.push('/')
             }
@@ -45,7 +72,32 @@
             },
             usercenter(){
             this.$router.push('/center')
+            },
+            async getdata(){
+                const res = await this.$http.get('data')
+                const {data} = res.data
+                        this.box1 = data[0]
+                        this.box2 = data[1]
+                        this.box3 = data[3]
+            },
+            onshow(){
+                this.show3 = true
+                this.$http.get('data')
+                .then(res=>{
+                    this.box1 = res['data'][0]
+                    this.box2 = res['data'][1]
+                    this.box3 = res['data'][2]
+                })
+                .catch(err=>{
+                    console.log('err')
+                })
+            },
+            show(){
+                this.show3 = false
+                setTimeout(this.onshow,100)
+                
             }
+            
         }
     }
 </script>  
@@ -57,6 +109,7 @@
     box-shadow: darkgrey 1px 1px 1px 1px;
 }
 .wall{
+    position: absolute;
     float: left;
     margin-left: 25px;
     height: 40px;
@@ -71,6 +124,7 @@
     vertical-align: middle;
 }
 .logout{
+    position:inherit;
     height: 40px;
     text-align: right;
     font-size: 17.7px;
@@ -78,16 +132,39 @@
     margin-right: 17px;
 }
 .usermodel{
-    margin-right: 15.7px;
+    margin-right: 10px;
 }
 .contribute{
-    margin-right: 15.7px;
+    margin-right: 10px;
     height: 32px;
     width: 70px;
     line-height: 32px;
     background-color: pink;
-    font-size: 17px;
+    font-size: 17.7px;
     color: #ffffff;
     border:none;
+}
+ .transition-box {
+    margin: 0 auto;
+    margin-bottom: 10px;
+    width: 20%;
+    height: 200px;
+    border-radius: 4px;
+    background-color: #409EFF;
+    text-align: center;
+    color: #fff;
+    padding: 40px 20px;
+    box-sizing: border-box;
+    /* margin-right: 20px; */
+  }
+.changemsg{
+    margin-top: 205px;
+    float: right;
+    display: block;
+}
+.msgmoudel{
+    height: 500px;
+    width: 100%;
+    /* background-color: gold; */
 }
 </style>
--- src/page/index.vue
+++ src/page/index.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="login-warp">
+     
     <el-form class="login-form" label-position="right"
         label-width="60px" 
-        :model="formdata">
+        :model="formdata" >
+         <span class="register">注册</span>
       <h3>用户登陆</h3>
   <el-form-item label="账号">
     <el-input v-model="formdata.username"></el-input>
@@ -13,6 +15,7 @@
    <el-button type="primary" plain 
    @click.prevent = "handellogin"
    class="login-btn">登陆</el-button>
+
 </el-form>
     </div>
 </template>
@@ -27,29 +30,51 @@ export default {
     }
       },
       methods:{
-        handellogin(){
+        open1() 
+        {
+        
+        },
+        handellogin()
+        {
           this.$http.post('login',this.formdata)
           .then(res=>{
-            console.log(res)
-            const {
+          const {
               data,
               meta:{msg,status}
-            } = res.data
-            if(status === 200){
-              this.$router.push('/home')
-            }else{
-              alert('账号或密码错误')
+          } = res.data
+          if(status === 200){
+              this.$cookieStore.setCookie('username',this.formdata.username,10)
+              this.$router.push({'name':'home'})
+              this.$message({
+              showClose: true,
+              message: msg,
+              type: 'success'
+              });
+          }
+          else{
+              this.$message({
+              showClose: true,
+              message: msg,
+              type: 'error'
+              });
             }
-           
           })
           .catch(err=>{
-            console.log('账号或密码错误')
+            this.$message({
+              showClose: true,
+              message: '账号或密码错误',
+              type: 'error'
+              });
           })
         }
-      }
+        },
 }
 </script>
 <style>
+body{
+    /* background: url("../assets/img/youname.jpg"); */
+
+}
 .login-warp{
   height: 100%;
   /* 弹性盒布局 */
@@ -64,10 +89,13 @@ export default {
 }
 .login-form{
   width: 317px;
-  background-color: cyan;
+  /* background-color: cyan; */
   border-radius: 5px;
   padding: 47.7px;
-  
+  background: url("../assets/img/youname.jpg");
+  background-size: 550px 325px;
+  background-position: center;
+  background-repeat:no-repeat;
 }
 .login-btn{
   border-radius: 5px;
@@ -75,5 +103,16 @@ export default {
   align-items: center;
   display: block;
   margin: 0 auto;
+}
+/* 注册模块 */
+.register{
+  display: block;
+  line-height: 20px;
+  text-align: right;
+  font-size: 18px;
+  font-family:'PingFang SC';
+  color: #606266;
+  padding: 0;
+  margin: 0;
 }
 </style>
